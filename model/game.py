c513b2f25d498100cd47e0a681627c9e4fd5d0cc
Slave <>
2021-06-25 19:28:39 +0500

    base description

--- model/game.py
+++ model/game.py
@@ -1,6 +1,18 @@
-class Game:
-    def __init__(self):
-        self._snake = Snake()
+from model.snake import Segment, Snake
 
-    def snakesMove(self):
-        pass
+class Game:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.snake = Snake((width - 1) // 2, (height - 1) // 2, (
+            Segment((width - 1) // 2, (height - 1) // 2 - 1)
+        ))
+
+    def step(self):
+        self.snakes_move()
+
+    def snakes_move(self):
+        self.snake.move()
+
+    def update_snakes_direction(self, direction):
+        self.snake.update_direction(direction)
